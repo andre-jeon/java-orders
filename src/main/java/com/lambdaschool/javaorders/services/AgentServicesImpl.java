d9bc9b6ab0a85933c232d79e8d2af2921d511560
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-09-08 23:11:17 -0400

    mvp done

---
+++ src/main/java/com/lambdaschool/javaorders/services/AgentServicesImpl.java
@@ -0,0 +1,17 @@
+package com.lambdaschool.javaorders.services;
+
+import com.lambdaschool.javaorders.models.Agent;
+import com.lambdaschool.javaorders.repositories.AgentsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service(value = "agentServices")
+public class AgentServicesImpl implements AgentServices{
+    @Autowired
+    AgentsRepository agentrepos;
+
+    @Override
+    public Agent save(Agent agent) {
+        return agentrepos.save(agent);
+    }
+}
